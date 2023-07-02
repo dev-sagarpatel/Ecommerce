505d113d70f35f05af850d95a06efa945c1c25cb
sagar ratanpara <dev.srpatel09@gmail.com>
2023-07-02 23:43:48 +0530

    responsive start

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -18,23 +18,4 @@ $(document).ready(function () {
             }
         }
     });
-
-    $('.owl-carousel .blog-articles').owlCarousel({
-        items: 3, 
-        margin: 40, 
-        loop: true, 
-        nav: false,
-        dots: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            1024: {
-                items: 3
-            }
-        }
-    });
 });
