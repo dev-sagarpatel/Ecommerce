3603d45e8eca3496be9cbe7be445943a9e074e0b
sagar ratanpara <dev.srpatel09@gmail.com>
2023-07-05 04:31:32 +0530

    home page header menu logic done and subheader form added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,71 @@
-const menuButton = document.querySelector(".menu-button");
-const closeButton = document.querySelector(".close-button");
-const menuModal = document.querySelector(".menu-modal");
-const menuModalLI = menuModal.querySelectorAll("li");
-const menuModalTwo = document.querySelector(".menu-modal-two");
-const menuModalTwoLi = menuModalTwo.querySelectorAll("li");
-const menuModalTwoButton = menuModalTwo.querySelector("button");
-const menuModalThree = document.querySelector(".menu-modal-three");
-const menuModalThreeButton = menuModalThree.querySelector("button");
+var isMainMenuActive = false;
+var isSubMenuActive = false;
+var isSubSubMenuActive = false;
 
-menuButton.addEventListener("click", () => {
-    menuButton.classList.add("hide");
-    closeButton.classList.remove("hide");
-    menuModal.classList.remove("hide");
+$(".menu-button").click(function () {
+  isMainMenuActive = true;
+  isSubMenuActive = false;
+  isSubSubMenuActive = false;
+  $(".menu-button").addClass("hide");
+  $(".close-button").removeClass("hide");
+  $(".menu-modal").removeClass("hide");
 });
 
-closeButton.addEventListener("click", () => {
-    menuButton.classList.remove("hide");
-    closeButton.classList.add("hide");
-    menuModal.classList.add("hide");
+$(".close-button").click(function () {
+  isMainMenuActive = false;
+  isSubMenuActive = false;
+  isSubSubMenuActive = false;
+  $(".menu-button").removeClass("hide");
+  $(".close-button").addClass("hide");
+  $(".menu-modal").addClass("hide");
+  $(".menu-modal-two").slideUp(300).removeClass("two-active");
+  $(".menu-modal-three").slideUp(300).removeClass("three-active");
 });
 
-// menuModalLI.forEach((li) => {
-//     li.addEventListener("click", () => {
-//         menuModalTwo.classList.remove("hide");
-//     });
-// });
+$(".menu-modal-two").parent("li").click(function () {
+  isSubMenuActive = true;
+  $(this).find(">.menu-modal-two").not(':animated').slideDown(300);
+  $(this).find(">.menu-modal-two").addClass("two-active");
+});
 
-// menuModalTwoButton.addEventListener("click", () => {
-//     menuModalTwo.classList.add("hide");
-// });
+$(".menu-modal-three").parent("li").click(function () {
+  isSubSubMenuActive = true;
+  $(".menu-modal-three.three-active").slideUp(300).removeClass("three-active");
+  $(this).find(">.menu-modal-three").not(':animated').slideDown(300);
+  $(this).find(">.menu-modal-three").addClass("three-active");
+});
 
-// menuModalTwoLi.forEach((li) => {
-//     li.addEventListener("click", () => {
-//         menuModalThree.classList.remove("hide");
-//     });
-// });
+$(".menu-modal-two > .common-title > button").click(function () {
+  isSubMenuActive = false;
+  isSubSubMenuActive = false;
+  $(".menu-modal-three").slideUp(300).removeClass("three-active");
+  $(".menu-modal-two").slideUp(300).removeClass("two-active");
+});
 
-// menuModalThreeButton.addEventListener("click", () => {
-//     menuModalThree.classList.add("hide");
-// });
+$(".menu-modal-three > .common-title > button").click(function () {
+  isSubSubMenuActive = false;
+  $(".menu-modal-three").slideUp(300).removeClass("three-active");
+});
 
+$(".main-menu-list").click(function () {
+  if (isSubMenuActive) {
+    if ($(this).find(">.menu-modal-two").length === 0) {
+      $(".menu-modal-three").slideUp(300).removeClass("three-active");
+      $(".menu-modal-two").slideUp(300).removeClass("two-active");
+    }
+  }
+})
+// sub header form script
+$("#rego").click(function () {
+  $(".mobile-vehicle.active").removeClass("active");
+  $(".mobile-rego").addClass("active");
+  $(".form-block.vehicle").slideUp(300).removeClass("active-form");
+  $(".form-block.rego").slideDown(300).addClass("active-form");
+})
 
+$("#vehicle").click(function () {
+  $(".mobile-vehicle").addClass("active");
+  $(".mobile-rego.active").removeClass("active");
+  $(".form-block.rego").slideUp(300).removeClass("active-form");
+  $(".form-block.vehicle").slideDown(300).addClass("active-form");
+})
